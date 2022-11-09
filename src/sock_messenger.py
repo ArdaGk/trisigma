ff579a9295231483e9a3480e2da2097926dbcc2f
Arda Gök <ardagk@Ardas-Air.lan>
2022-11-09 14:15:24 -0600

    socket messenger added

---
+++ src/sock_messenger.py
@@ -0,0 +1,11 @@
+import socket
+class Messenger:
+    def __init__ (self, addr, port):
+        self.addr = addr
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+        self.sock.connect((addr, port))
+    def send (self, msg):
+       self.sock.send(msg.encode())
+       resp = self.sock.recv(1024)
+       return reps
